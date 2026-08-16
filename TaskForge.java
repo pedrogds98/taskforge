0f57ec3443e20405aafa23fccad4ff456b07f8f1
Pedro Gabriel <jamis.gg@gmail.com>
2026-08-16 12:51:15 -0300

    Atualiza TaskForge

--- TaskForge.java
+++ TaskForge.java
@@ -9,17 +9,45 @@
 //   - No terminal: java TaskForge.java
 
 void main() {
-    IO.println("=== TaskForge v0 ===");
+    int option = 0;
 
-    String nome = IO.readln("Nome da tarefa: ");
-    String descricao = IO.readln("Descrição: ");
-    int prioridade = Integer.parseInt(IO.readln("Prioridade (1 a 5): "));
+    String name = "";
+    String descritor = "";
+    int priority = 0;
 
-    IO.println("");
-    IO.println("------ TAREFA CRIADA ------");
-    IO.println("Tarefa:     " + nome);
-    IO.println("Descrição:  " + descricao);
-    IO.println("Prioridade: " + prioridade);
-    IO.println("Status:     pendente");
-    IO.println("---------------------------");
+    while (option !=3) {
+        IO.println("=== MENU PRINCIPAL ===");
+        IO.println("OPÇÂO 1 CRIAR TAREFA");
+        IO.println("OPÇÂO 2 VER RESUMO");
+        IO.println("OPÇÃO 3 SAIR");
+        option = Integer.parseInt(IO.readln("DIGITE UMA OPÇÃO: "));
+
+
+        switch (option) {
+
+            case 1:
+                IO.println("CRAR TAREFA: ");
+                name = IO.readln("Nome: ");
+                descritor = IO.readln("Descrição: ");
+                priority = Integer.parseInt(IO.readln("Prioridade (1 a 5): "));
+                IO.println("TAREFA CRIADA COM SUCESSO!");
+                break;
+
+            case 2:
+                IO.println("Nome da Tarefa" + name);
+                IO.println("Descrição da Tarefa:" + descritor);
+                for (int i = 1; priority <= i; i++;);
+                IO.print("*");
+                    IO.println("Prioridade da Tarefa:" + priority);
+                break;
+
+
+            case 3:
+                IO.println("FIM! ATÈ A PRÓXIMA!");
+                break;
+
+            default:
+                IO.println("OPÇÂO INVÁLIDA");
+        }
+    }
 }
